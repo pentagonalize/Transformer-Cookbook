6bc7083bb33233b02f066f44076572d3e049dc23
kadarakos <kadar.akos@gmail.com>
2024-05-11 01:57:52 +0200

    sum

--- src/minmax.py
+++ src/minmax.py
@@ -8,17 +8,31 @@ min_input_layer = np.array(
         [0, 0, -1]
     ]
 )
+min_output_layer = np.array([1, -1, -1])
 max_input_layer = np.array(
     [
         [1, -1, -1],
         [0, 0, 1]
     ]
 )
-min_output_layer = np.array([1, -1, -1])
 max_output_layer = np.array([1, -1, 1])
+sum_input_layer = np.array(
+   [
+       [1, 0],
+       [-1, 0],
+       [0, 1],
+       [0, -1],
+   ]
+).T
+sum_output_layer = np.array([1, -1, 1, -1])
 min_ff = FF(min_input_layer, min_output_layer)
 max_ff = FF(max_input_layer, max_output_layer)
+sum_ff = FF(sum_input_layer, sum_output_layer)
 print(min_ff([89, 98]))
 print(max_ff([59, 98]))
 print(min_ff)
 print(max_ff)
+print(sum_ff([89, 98]))
+print(sum_ff)
+print(sum_ff([-59, 98]))
+print(sum_ff)
